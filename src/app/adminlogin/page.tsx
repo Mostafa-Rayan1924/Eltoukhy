aec0b74c60793ae4962ebe21da79447a64575900
Mostafa rayan <mr3309296@gmail.com>
2025-01-15 15:07:53 +0200

    add login page

---
+++ src/app/adminlogin/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { formSchema } from "@/components/validation/LoginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+const Admin = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+  });
+  const { reset, formState } = form;
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    reset();
+  }
+  return (
+    <section className="mt-[150px] container">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 max-w-[600px] mx-auto border border-border p-4 rounded-lg">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your Email"
+                    {...field}
+                    value={field.value || ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your Password"
+                    {...field}
+                    value={field.value || ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className={"w-full"} type="submit">
+            {formState.isSubmitting ? "loading" : "Submit"}
+          </Button>
+        </form>
+      </Form>
+    </section>
+  );
+};
+
+export default Admin;
---
+++ src/components/validation/LoginSchema.ts
@@ -0,0 +1,12 @@
+"use client";
+
+import { z } from "zod";
+
+export const formSchema = z.object({
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Invalid email address" }),
+  password: z
+    .string({ message: "Password is required" })
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
